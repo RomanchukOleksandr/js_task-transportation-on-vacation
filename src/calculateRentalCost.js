ee4a88de90eae2d24245a256c9239c9ac24e639a
RomanchukOleksandr <oleksandr.romanchukk@gmail.com>
2025-03-05 12:51:13 +0200

    Add changes

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,13 +5,18 @@
  */
 function calculateRentalCost(days) {
   let sum = 0;
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_DISCOUNT = 20;
+  const LONG_TERM = 7;
+  const SHORT_TERM = 3;
 
-  if (days >= 7) {
-    sum = 40 * days - 50;
-  } else if (days >= 3) {
-    sum = 40 * days - 20;
+  if (days >= LONG_TERM) {
+    sum = PRICE_PER_DAY * days - LONG_TERM_DISCOUNT;
+  } else if (days >= SHORT_TERM) {
+    sum = PRICE_PER_DAY * days - SHORT_TERM_DISCOUNT;
   } else {
-    sum = 40 * days;
+    sum = PRICE_PER_DAY * days;
   }
 
   return sum;
